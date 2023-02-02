353565fb8f5be9bbfd5de419e070345a7800e03e
senay berhe <tsionberhe@gmail.com>
2023-02-01 16:20:14 -0800

    added files

--- config/settings.py
+++ config/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = env('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env('DEBUG')
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['web-production-ce8a.up.railway.app', '127.0.0.1' ]
 
 
 # Application definition
