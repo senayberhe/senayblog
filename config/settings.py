79299ec85d3424b8795fb45e0382b4996b3a3843
senay berhe <tsionberhe@gmail.com>
2023-02-01 16:44:37 -0800

    added files

--- config/settings.py
+++ config/settings.py
@@ -27,7 +27,7 @@ DEBUG = env('DJANGO_DEBUG')
 
 ALLOWED_HOSTS = ['web-production-ce8a.up.railway.app', '127.0.0.1' ]
 
-CSRF_TRUSTED_ORIGINS = ['https://web-production-ce8a.up.railway.app']
+# CSRF_TRUSTED_ORIGINS = ['https://web-production-ce8a.up.railway.app']
 # Application definition
 
 INSTALLED_APPS = [
